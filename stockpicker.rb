93ca29ed582fbb11d9c39812dd685c1eb1e77049
Mike Rachman <mikerachman1@gmail.com>
2022-05-27 13:13:41 -0400

    It all seems to be working now

--- stockpicker.rb
+++ stockpicker.rb
@@ -1,21 +1,29 @@
 stocks = [17,3,6,9,15,8,6,1,10]
+stocks2 = [5, 7, 17, 6, 9, 1]
 
 def stock_picker(prices)
     buy_day = nil
     sell_day = nil
     results = []
+    profits = [] 
     
     prices.each do |price|
-        
         i = prices.index(price)
         while i < (prices.length)-1 do
             i += 1
-            #profit = 0
             profit = price - prices[i]
             results.push([price, prices[i], profit])
         end
     end
-    p results
+    results.each do |group|
+        profits.push(group[2])   
+    end
+    max_profits = profits.min
+    max_profits_array = results.select {|group| group.include?(max_profits)}
+    max_profits_array.flatten!.pop
+    max_profit_days = []
+    max_profit_days.push(prices.index(max_profits_array[0]), prices.index(max_profits_array[1]))
+    p max_profit_days
 end
 
-stock_picker(stocks)
+stock_picker(stocks2)
